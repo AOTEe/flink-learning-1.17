9ca31ae9fe1ee0aa04543b2b233ded637a8f057e
aootz <1437266146@qq.com>
2025-02-09 20:23:14 +0800

    operator chain

--- src/main/java/com/aootz/wc/UnboundedStreamOperatorChainDemo.java
+++ src/main/java/com/aootz/wc/UnboundedStreamOperatorChainDemo.java
@@ -8,13 +8,13 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class WordCountStreamUnboundedDemo {
+public class UnboundedStreamOperatorChainDemo {
     public static void main(String[] args) throws Exception {
 
         // 1 创建执行环境
         //StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
-        // env.setParallelism(30);
+        env.setParallelism(6);
         // 2 读取数据： socket linux：nc -lk 9000
         DataStreamSource<String> source = env.socketTextStream("192.168.93.136", 9000);
         // 3. 处理数据
@@ -24,7 +24,7 @@ public class WordCountStreamUnboundedDemo {
                         out.collect(Tuple2.of(word, 1));
                     }
                 })
-                //.setParallelism(20)// 设置算子平行度，并行度优先级：算子>执行环境>运行时指定
+                .setParallelism(2)// 设置算子平行度，并行度优先级：算子>执行环境>运行时指定
                 .returns(Types.TUPLE(Types.STRING, Types.INT))
                 .keyBy(value -> value.f0)// 不设置并行度，本地模式默认为电脑的线程数
                 .sum(1);
@@ -35,4 +35,23 @@ public class WordCountStreamUnboundedDemo {
         // 5. 执行
         env.execute();
     }
+
+    /**
+     1、算子之间的传输关系：
+     一对一
+     重分区
+     2、算子 串在一起的条件：
+     1）一对一
+     2）并行度相同
+     3、关于算子链的api：
+     1）全局禁用算子链：env.disableOperatorChaining();
+     2）某个算子不参与链化：算子A.disableChaining(),算子A不会与 前面 和 后面 的算子 串在一起
+     3）从某个算子开启新链条：算子A.startNewChain(),算子A不与 前面 串在一起，后面的可以串在一起
+
+
+
+     什么时候禁用算子链（大多数情况下不需要禁用）
+     1、各自算子链的运算任务很重
+     2、定位问题，排查具体的算子的问题
+     */
 }
